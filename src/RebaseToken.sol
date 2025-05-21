755bbee73a8ed09a084fa0bc475590bdec3b708f
saikrishnacret <saikrishnaask191@gmail.com>
2025-05-21 16:17:03 +0530

    tests start

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -18,9 +18,9 @@ contract RebaseToken is ERC20, Ownable , AccessControl {
     //////////////////////////////////////////
     error RebaseToken__InterestRateCanOnlyDecrease(uint256 s_interestRate, uint256 _newInterestRate);
 
-    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private constant PRECISION_FACTOR = 1e27;
     bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
-    uint256 private s_interestRate = 5e10;
+    uint256 private s_interestRate =(5 * PRECISION_FACTOR)/1e8;
     mapping(address => uint256) s_userInterestRate;
     mapping(address => uint256) s_userLastUpdatedTimestamp;
 
